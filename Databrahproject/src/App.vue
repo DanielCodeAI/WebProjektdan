08ae885323339d07212023f97af49d6ae3c98de6
danielbonath <bonathd@dhbw-loerrach.de>
2024-10-02 23:51:22 +0200

    Initial commit

---
+++ Databrahproject/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+
+//////
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  mounted() {
+    // Load Google API client library
+    gapi.load('client:auth2', () => {
+      gapi.client.init({
+        apiKey: 'YOUR_GOOGLE_API_KEY',
+        clientId: 'YOUR_GOOGLE_CLIENT_ID',
+        discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+        scope: 'https://www.googleapis.com/auth/spreadsheets',
+      }).then(() => {
+        console.log("Google API initialized");
+      });
+    });
+  }
+};
+</script>
+
---
+++ Databrahproject/src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <button @click="signIn">Login with Google</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      signIn() {
+        gapi.auth2.getAuthInstance().signIn().then((googleUser) => {
+          const profile = googleUser.getBasicProfile();
+          const id_token = googleUser.getAuthResponse().id_token;
+          // You can save the token in the local storage
+          localStorage.setItem('googleToken', id_token);
+          this.$router.push('/');
+        });
+      }
+    }
+  };
+  </script>
+  
